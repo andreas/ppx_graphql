fe41250bcf82dc384ac79e044f1283eadca6f2db
Andreas Garnaes <andreas.garnaes@gmail.com>
2018-02-03 14:05:31 +0100

    Update github example to use union

--- examples/github.ml
+++ examples/github.ml
@@ -6,7 +6,7 @@ let executable_query (query, kvariables, parse) =
         let uri = Uri.of_string "https://api.github.com/graphql" in
         let headers = Cohttp.Header.of_list [
           "Authorization", "bearer " ^ token;
-          "User-Agent", "andreas/ocaml-graphql";
+          "User-Agent", "andreas/ppx_graphql";
         ] in
         let body = `Assoc [
           "query", `String query;
@@ -14,7 +14,7 @@ let executable_query (query, kvariables, parse) =
         ] in
         let serialized_body = Yojson.Basic.to_string body in
         Cohttp_lwt_unix.Client.post ~headers ~body:(`String serialized_body) uri >>= fun (rsp, body) ->
-        Cohttp_lwt_body.to_string body >|= fun body' ->
+        Cohttp_lwt.Body.to_string body >|= fun body' ->
         match Cohttp.Code.(code_of_status rsp.status |> is_success) with
         | false ->
             Error body'
@@ -35,16 +35,32 @@ let find_repository = executable_query [%graphql {|
   }
 |}]
 
+let search_repositories = executable_query [%graphql {|
+  query SearchRepositories($query: String!) {
+    search(query: $query, type: REPOSITORY, first: 5) {
+      nodes {
+        __typename
+        ...on Repository {
+          nameWithOwner
+        }
+      }
+    }
+  }
+|}]
+
 let main () =
   let token = Unix.getenv "GITHUB_TOKEN" in
-  find_repository ~token ~owner:"andreas" ~name:"ocaml-graphql-server" () >|= function
+  search_repositories ~token ~query:"topic:mirageos" () >|= function
   | Ok rsp ->
-      begin match rsp#repository with
-      | Some repo ->
-          Format.printf "Repo id: %s" repo#id
-      | None ->
-          Format.printf "Repo not found"
-      end
+    begin match rsp#search#nodes with
+    | Some nodes ->
+      List.iter (function
+        | Some (`Repository r) -> Format.printf "Repo: %s\n" r#nameWithOwner
+        | _ -> ()
+      ) nodes
+    | None ->
+      Format.printf "Empty search result"
+    end
   | Error err ->
       Format.printf "Error! %s" err
 
